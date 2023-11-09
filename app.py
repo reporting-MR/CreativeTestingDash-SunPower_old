9137b5a06f14092791ad59a0d840719f6ce98240
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-11-09 17:21:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def main_dashboard():
 
   data = st.session_state.full_data
   data = data[data['Ad_Set_Name__Facebook_Ads'] == 'T1-T3_Adults-25+1DC_Batch-26-Shelbi-Repurposed-Test-102423']
-  data = data.groupby('Ad_Name__Facebook_Ads').sum()
+  data = data.groupby('Ad_Name__Facebook_Ads')
+  aggregated_df = data.sum()
   st.write(data)
 
 if __name__ == '__main__':
