fe930b71dad3a21e120277a3cb94e3ca0241dcf7
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-11-09 16:22:50 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas_gbq
+import pandas 
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+def password_protection():
+  if 'authenticated' not in st.session_state:
+      st.session_state.authenticated = False
+      
+  if not st.session_state.authenticated:
+      password = st.text_input("Enter Password:", type="password")
+      correct_hashed_password = "Sunpower1234"
+      
+      if st.button("Login"):
+          if password == correct_hashed_password:
+              st.session_state.authenticated = True
+              main_dashboard()
+          else:
+              st.error("Incorrect Password. Please try again or contact the administrator.")
+  else:
+      main_dashboard()
+
+def main_dashboard():
+  st.markdown("<h1 style='text-align: center;'>SunPower Creative Ad Testing</h1>", unsafe_allow_html=True)
+  # Calculate the date one year ago from today
+  one_year_ago = (datetime.now() - timedelta(days=365)).date()
+  
+  if 'full_data' not in st.session_state:
+      credentials = service_account.Credentials.from_service_account_info(
+          st.secrets["gcp_service_account"]
+      )
+      client = bigquery.Client(credentials=credentials)
+      # Modify the query
+      query = f"""
+      SELECT * FROM `sunpower-375201.sunpower_segments.sunpower_platform_ad_level` 
+      WHERE Date BETWEEN '{one_year_ago}' AND CURRENT_DATE() """
+      st.session_state.full_data = pandas.read_gbq(query, credentials=credentials)
+
+  st.write(st.session_state.full_data)
+
+    
+
+    
