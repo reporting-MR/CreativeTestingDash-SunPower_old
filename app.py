e7aec14b118eeacb9229235aa802d614e8c2b5f5
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-11-13 11:33:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def main_dashboard():
       st.session_state.full_data = pandas.read_gbq(query, credentials=credentials)
 
   data = st.session_state.full_data
-  #data = data[data['Ad_Set_Name__Facebook_Ads'] == 'T1-T3_Adults-25+1DC_Batch-26-Shelbi-Repurposed-Test-102423']
+  data = data[data['Ad_Set_Name__Facebook_Ads'] == 'T1-T3_Adults-25+1DC_Batch-26-Shelbi-Repurposed-Test-102423']
   #data = data.groupby('Ad_Name__Facebook_Ads')
   #aggregated_df = data.sum()
   st.write(data)
