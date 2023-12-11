00e14b8d6d4edb7d7cd33a023d16cfdd7874bced
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-12-11 10:04:29 -0800

    Update app.py

--- app.py
+++ app.py
@@ -431,10 +431,10 @@ def main_dashboard():
       # Upload the file
       response = upload_file_to_github(uploaded_file, file_path, token)
 
-      print(f"Token: {token[:4]}...")  # Prints first few characters for verification
-      print(f"File path: {file_path}")
-      print(f"Response status code: {response.status_code}")
-      print(f"Response content: {response.content}")
+      st.write(f"Token: {token[:4]}...")  # Prints first few characters for verification
+      st.write(f"File path: {file_path}")
+      st.write(f"Response status code: {response.status_code}")
+      st.write(f"Response content: {response.content}")
 
 
       if response.status_code == 201:
