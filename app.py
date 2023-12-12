0d91fedf429feda6731e7458f413d1814640e660
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-12-12 10:49:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -374,7 +374,10 @@ def main_dashboard():
 
 
   current_Ad_Set = current_test_data['Ad_Set'].iloc[0]
+  st.write(current_Ad_Set)
+  st.write(data)
   data = data[data['Ad_Set'] == current_Ad_Set]
+
 
   st.write(data)
 
