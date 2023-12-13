02abe8674e2246be81059f434e082c9113de60d5
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-12-13 14:48:45 -0800

    Update app.py

--- app.py
+++ app.py
@@ -321,6 +321,7 @@ def main_dashboard():
 
   past_test_data = st.session_state.past_test_data
   past_test_data['Ad_Set'] = past_test_data['Ad_Set'].apply(lambda x: x.strip("'"))
+  past_test_data = past_test_data.iloc[::-1].reset_index(drop=True)
   
   # Renaming columns in a DataFrame
   data = data.rename(columns={
