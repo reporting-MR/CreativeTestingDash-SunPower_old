63c8dda9fe672f7d947795524485832e938dfea7
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-12-21 10:20:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pandas_gbq
 import pandas 
+import os
 from google.oauth2 import service_account
 from google.cloud import bigquery
 from datetime import datetime, timedelta
